d982a38ed019720fb3722211cfe01f5842195c16
James <jamesbellairs@gmail.com>
2019-12-12 11:43:44 +0200

    day 6 part 1

--- app/solutions/day6/part1.ts
+++ app/solutions/day6/part1.ts
@@ -1,6 +1,31 @@
-import OpcodeComputer from "../opcodeComputer";
 import Solution from "../solution";
 import { getFileLines } from "../../api/providers/fileProvider";
+
+class OrbitalObject {
+    
+    public readonly name: string;
+    public parent?: OrbitalObject;
+
+    constructor(name: string, parent?: OrbitalObject) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public setParent = async (parent: OrbitalObject): Promise<void> => {
+        if (this.parent !== undefined)
+            throw new Error(`Unable to set parent to ${parent}. Parent is already defined as ${this.parent}`);
+
+        this.parent = parent;
+    }
+
+    public countObjectsInOrbit(): number
+    {
+        if (this.parent === undefined)
+            return 0;
+        
+        return 1 + this.parent.countObjectsInOrbit();
+    }
+}
 
 class Day6Part1 extends Solution {
 
@@ -8,17 +33,37 @@ class Day6Part1 extends Solution {
 
     public async computeAsync(): Promise<string> {
 
-        const lines = await getFileLines("app/solutions/day6/input");
+        const orbits = await getFileLines("app/solutions/day6/input");
+        const objects: OrbitalObject[] = [];
 
-        const orbits = lines[0].split(',').map(x => parseInt(x));
-        
+        // AAA)BBB
+        orbits.forEach(orbit => {
+            const [ parentName, childName ] = orbit.split(')');
 
-      
-        return "wooo! orbits!";
-        
+            const existingParent = objects.find(object => object.name === parentName);
+            const parent = existingParent || new OrbitalObject(parentName);
+
+            const existingChild = objects.find(object => object.name === childName);
+            const child = existingChild || new OrbitalObject(childName, parent);
+
+            // child may only have 1 parent
+            if (existingChild && existingChild.parent !== undefined)
+                throw new Error("This child was already defined! Name: "+childName)
+            
+            if (existingChild)
+                existingChild.setParent(parent);
+            else
+                objects.push(child);
+
+            if (!existingParent)
+                objects.push(parent);
+        });
+        console.log(objects.map(x => { return "Name: " + x.name + ", ParentName: " + (x.parent || {}).name }))
+        return objects.reduce((total, x) => total + x.countObjectsInOrbit(), 0).toString();        
     }
 }
 
 var solution = new Day6Part1();
 
 export default solution.computeAsync;
+
